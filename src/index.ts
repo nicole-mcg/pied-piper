d10d335b32741d6d1442a466648bdd2658db85f7
Connor <c.mcg132@gmail.com>
2018-12-06 13:00:58 -0500

    Removed unused code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 const uuidv4 = require('uuid/v4');
 
-import * as fs from 'fs'
 
 import App from "./App"
 
@@ -9,84 +8,12 @@ const app:App = new App();
 
 app.createRoute('/', onWebRequest);
 
-const server:any = app.httpServer;
-const io:any = app.io;
-
-io.on('connection', onConnectionRecieved);
-server.listen(3000);
+app.start();
 
 function onWebRequest(req:any, res:any) {
     res.sendFile("/public/index.html", { root: __dirname + "/../"});
    //res.sendFile(__dirname + "/file.json");
 }
 
-const sockets:object = {};
-
-function onConnectionRecieved(socket:any) {
-    console.log("Connection recieved");
-
-    const socketId:string = uuidv4();
-    socket.on('disconnect', () => onDisconnect(socketId));
-    socket.on('update', (payload) => updateFile(socketId, payload));
-
-    sockets[socketId] = socket;
-}
-
-function onDisconnect(socketId:string) {
-    if (!sockets[socketId]) {
-        console.warn("Unknown socket disconnected", socketId);
-        return;
-    }
-
-    delete sockets[socketId];
-}
-
-function onStart() {
-    console.log('listening on *:3000');
-}
-
-const ALLOW_TEXT_MESSAGES = false;
-function updateFile(socketId:string, payload:string) {
-    const socket:any = sockets[socketId];
-
-    if (ALLOW_TEXT_MESSAGES) {
-        io.emit('update', payload);
-        return;
-    }
-    try {
-        payload = JSON.parse(payload);
-    } catch (e) {
-        emitError(socket, "Invalid request payload");
-        return;
-    }
-
-    const newPayload = {};
-    const fileContents = JSON.stringify(payload);
-
-    fs.mkdir(__dirname + "/../data", (err:any) => {
-        if(err && err.code != "EEXIST") {
-            console.log("Error updating file", err)
-            emitError(socket, "Error saving updates");
-            return;
-        }
-        fs.writeFile(__dirname + "/../data/file.json", fileContents, (err:any) => {
-            if(err) {
-                console.log("Error updating file", err)
-                emitError(socket, "Error saving updates");
-                return;
-            }
-        
-            io.emit('update', fileContents);
-        }); 
-    })
-
-    
-
-}
-
-function emitError(socket:any, message:string) {
-    const payload = {
-        error: message
-    };
-    socket.emit("update_error", JSON.stringify(payload));
-}
+//     fs.mkdir(__dirname + "/../data"
+//     fs.writeFile(__dirname + "/../data/file.json"
