55d6077812da5c8b01f4d0738e9061fd3c13dce9
Connor <c.mcg132@gmail.com>
2018-12-06 17:03:25 -0500

    Removed routes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 const uuidv4 = require('uuid/v4');
 
 
-import App from "./App"
-
+import App from "./App";
 
 const app:App = new App();
-
-app.createRoute('/', onWebRequest);
-
 app.start();
-
-function onWebRequest(req:any, res:any) {
-    res.sendFile("/public/index.html", { root: __dirname + "/../"});
-   //res.sendFile(__dirname + "/file.json");
-}
