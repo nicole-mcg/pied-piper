940024f0ebf112ecca248768f5a81c65b57ae4af
Connor <c.mcg132@gmail.com>
2018-12-06 07:47:49 -0500

    Added RouteHandler type

--- src/routes/Route.ts
+++ src/routes/Route.ts
@@ -1,9 +1,12 @@
+import '../types'
+
+import App from '../App'
 
 export default class Route {
     public path:string;
-    public handler:Function;
+    public handler:RouteHandler;
 
-    constructor(path:string, handler:Function) {
+    constructor(path:string, handler:RouteHandler) {
         this.path = path;
         this.handler = handler;
     }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 declare global {
     interface Route {}
+    type RouteHandler = (req:any, res:any, app:App) => any;
     interface App {
         createRoute(path:string, handlerFunc:(req:any, res:any, app:App) => any):void,
         createRoutes(routes:Array<Route>):void,
