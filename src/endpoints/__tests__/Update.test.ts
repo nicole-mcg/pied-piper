043a99212e7f2dcac24723c148989572f38840ce
Connor <c.mcg132@gmail.com>
2018-12-07 09:06:43 -0500

    cleaned up test

--- src/endpoints/__tests__/Update.test.ts
+++ src/endpoints/__tests__/Update.test.ts
@@ -2,9 +2,10 @@ import fs from 'fs'
 import path from 'path'
 
 import MockSocket from '../../socket/Socket'
+
+import Endpoint from '../AbstractEndpoint';
 import UpdateEndpoint from '../Update';
 
-jest.mock('../../socket/Socket');
 jest.mock('fs', () => {
     let error:any = null;
     return {
@@ -19,50 +20,46 @@ jest.mock('fs', () => {
     }
 });
 
+jest.mock('../../socket/Socket');
+
 describe('UpdateEndpoint', () => {
 
     const parentDir = path.normalize(__dirname+"/..");
     const dirPath = parentDir + "/../../data";
     const filePath = parentDir + "/../../data/file.json";
 
-    let updateEndpoint:any;
-    let mockServer;
-    let mockSocket;
+    const mockSocket = new MockSocket({}, null);
+    const mockServer = { 
+        io: { emit: jest.fn() }
+    }
 
-    const payload = "{}";
+    let updateEndpoint:any;
 
     beforeEach(() => {
         updateEndpoint = new UpdateEndpoint();
-        mockServer = {
-            io: {
-                emit: jest.fn(),
-            }
-        };
-        mockSocket = new MockSocket({}, mockServer.app);
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     })
 
-    it('can recieve an update from a socket', (done) => {
-        try {
-            updateEndpoint.handleEndpoint(payload, mockSocket, mockServer)
-            expect(fs.existsSync).toHaveBeenCalledWith(dirPath);            
-            expect(fs.mkdirSync).toHaveBeenCalledWith(dirPath);
-            expect(fs.writeFileSync).toHaveBeenCalledWith(filePath, payload);
-            expect(mockServer.io.emit).toHaveBeenCalledWith('update', payload);
-            done();
-        } catch(e) {
-            done.fail(e);
-        }
+    it('can recieve an update from a socket', () => {
+        (fs as any).shouldFail(false);
+        const payload = "{}";
+        
+        updateEndpoint.handleEndpoint(payload, mockSocket, mockServer)
+
+        expect(fs.existsSync).toHaveBeenCalledWith(dirPath);            
+        expect(fs.mkdirSync).toHaveBeenCalledWith(dirPath);
+        expect(fs.writeFileSync).toHaveBeenCalledWith(filePath, payload);
+        expect(mockServer.io.emit).toHaveBeenCalledWith('update', payload);
     });
 
     it('will emit error to socket on update failure', (done) => {
-
         (fs as any).shouldFail();
         try {
-            updateEndpoint.handleEndpoint(payload, mockSocket, mockServer)
+            updateEndpoint.handleEndpoint("{}", mockSocket, mockServer)
+
             expect(fs.existsSync).toHaveBeenCalledWith(dirPath);
             expect(mockSocket.emitError).toHaveBeenCalled();
             expect(console.log).toHaveBeenCalled();
