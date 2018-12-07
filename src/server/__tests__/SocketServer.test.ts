04260be54f249b40d73854556d4294a5d93471e3
Connor <c.mcg132@gmail.com>
2018-12-06 20:52:36 -0500

    Updated tests

--- src/server/__tests__/SocketServer.test.ts
+++ src/server/__tests__/SocketServer.test.ts
@@ -3,6 +3,8 @@ import http from 'http'
 
 import SocketServer from '../SocketServer'
 
+import Socket from './../Socket';
+
 jest.mock('express');
 jest.mock('http');
 jest.mock('../Socket');
@@ -16,6 +18,9 @@ jest.mock('socket.io', () => (
 ));
 
 describe('SocketServer', () => {
+    const socket:any = {}
+    const endpoint:any = {}
+    const payload:string = "{}";
 
     let socketServer:any = null;
     let mockHttpServer:any;
@@ -23,6 +28,9 @@ describe('SocketServer', () => {
     beforeEach(() => {
         mockHttpServer = new http.Server();
         socketServer = new SocketServer(mockHttpServer, {});
+
+        endpoint.handleEndpoint = jest.fn();
+        socket.emitError = jest.fn();
     });
 
     afterEach(() => {
@@ -31,6 +39,7 @@ describe('SocketServer', () => {
 
     it('can be created', () => {
         expect(socketServer).toBeTruthy();
+        expect(socketServer.endpoints).toEqual({});
         expect(socketServer.io).toBeTruthy();
         expect(ioStub.on).toBeCalledWith('connection', socketServer.onConnectionRecieved);
     });
@@ -40,41 +49,51 @@ describe('SocketServer', () => {
         console.log = jest.fn();
 
         const ioSocketStub = {};
-
-        const socket = socketServer.onConnectionRecieved(ioSocketStub);
+        const recievedSocket = socketServer.onConnectionRecieved(ioSocketStub);
 
         // Verify we got a mock socket
-        expect(socket).toHaveProperty('id', 'test');
+        expect(Socket).toHaveBeenCalled();
+        expect(recievedSocket).toHaveProperty('id', 'test');
 
         expect(console.log).toHaveBeenCalled();
         console.log = oldLog;
     });
 
-    it('can validate a payload', () => {
+    it('can accept a valid payload', () => {
         const validPayload = "{}";
         expect(socketServer.validatePayload(validPayload)).toBe(true);
+    });
 
+    it('can deny an invalid payload', () => {
         const invalidPayload = "invalid json";
         expect(socketServer.validatePayload(invalidPayload)).toBe(false);
     });
 
     it('can handle an endpoint', () => {
-        const payload = "{}";
-        const testEndpoint = {
-            handleEndpoint: jest.fn(),
-        }
-        const socket = {};
         socketServer = new SocketServer(mockHttpServer, {
-            test: testEndpoint,
+            test: endpoint,
         });
         socketServer.validatePayload = jest.fn().mockReturnValue(true);
         
         socketServer.handleEndpoint('test', payload, socket);
 
-        expect(testEndpoint.handleEndpoint).toHaveBeenCalledWith(payload, socket, socketServer);
-        expect(socketServer.validatePayload).toHaveBeenCalledWith(payload);
+        expect(endpoint.handleEndpoint)
+            .toHaveBeenCalledWith(payload, socket, socketServer);
+        expect(socketServer.validatePayload)
+            .toHaveBeenCalledWith(payload);
     });
 
-    
+    it('wont handle an endpoint if invalid payload', () => {
+        socketServer = new SocketServer(mockHttpServer, {
+            test: endpoint,
+        });
+        socketServer.validatePayload = jest.fn().mockReturnValue(false);
+        
+        socketServer.handleEndpoint('test', payload, socket);
+
+        expect(endpoint.handleEndpoint).not.toHaveBeenCalled();
+        expect(socketServer.validatePayload).toHaveBeenCalledWith(payload);
+        expect(socket.emitError).toHaveBeenCalled();
+    });
 
 });
