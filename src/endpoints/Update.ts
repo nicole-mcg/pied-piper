4548fb7116f5f27647b83191c49deb381832d7a1
Connor <c.mcg132@gmail.com>
2018-12-07 18:31:19 -0500

    Add get method to Update endpoint

--- src/endpoints/Update.ts
+++ src/endpoints/Update.ts
@@ -8,29 +8,39 @@ import Endpoint from './Endpoint'
 export default class UpdateEndpoint extends Endpoint {
 
     get(payload:string, client:Client, server:SocketServer) {
-        if (!payload) {
-            if (!fs.existsSync(DATA_FILE_PATH)) {
-                client.onError("Error loading file");
-                return;
-            }
-
-            const fileContents = fs.readFileSync(DATA_FILE_PATH);
-            client.onSuccess(fileContents.toString());
+        if (!fs.existsSync(DATA_FILE_PATH)) {
+            client.onError("Error loading file");
             return;
         }
 
-        try {
-            if (!fs.existsSync(DATA_DIR_PATH)){
-                fs.mkdirSync(DATA_DIR_PATH);
-            }
+        let fileContents = fs.readFileSync(DATA_FILE_PATH).toString();
 
-            fs.writeFileSync(DATA_FILE_PATH, payload)
-            server.io.emit('update', payload);
-            client.onSuccess(payload);
-        } catch (error) {
-            console.log(`Error saving file: ${error}`)                
-            client.onError("Error saving data");
+        try {
+            fileContents = JSON.parse(fileContents);
+        } catch (e) {
+            client.onError("Error loading file");
+            return;
         }
+
+        if (!payload) {           
+            client.onSuccess(JSON.stringify(fileContents));
+            return;
+        }
+
+        let key;
+        try {
+            key = JSON.parse(payload).key
+        } catch (e) {
+            client.onError("Invalid key");
+            return;
+        }
+
+        if (!fileContents[key]) {
+            client.onError("Key does not exist");
+            return;
+        }
+
+        client.onSuccess(JSON.stringify(fileContents[key]));
     }
     
     put(payload:string, client:Client, server:SocketServer) {
--- src/endpoints/__tests__/Update.test.ts
+++ src/endpoints/__tests__/Update.test.ts
@@ -6,16 +6,36 @@ import UpdateEndpoint from '../Update';
 
 
 jest.mock('fs', () => {
+    let doesFileExist = true;
+    let isFileValid = true;
     let error:any = null;
     return {
-        shouldFail: (err={}) => error = err,
-        existsSync: jest.fn().mockImplementation((path) => {
+        throwsError: (err={}) => {
+            error = err
+            return fs;
+        },
+        fileExists: (exists=true) => {
+            doesFileExist = exists
+            return fs;
+        },
+        
+        fileIsValid: (valid=true) => {
+            isFileValid = valid;
+            return fs;
+        },
+        restore: () => {
+            doesFileExist = true;
+            isFileValid = true;
+            error = null;
+        },
+        existsSync: jest.fn().mockImplementation((path) => doesFileExist),
+        readFileSync: jest.fn().mockImplementation((path) => isFileValid ? '{"test": 1}' : "err"),
+        mkdirSync: jest.fn(),
+        writeFileSync: jest.fn().mockImplementation((path) => {
             if (error) {
                 throw new Error();
             }
-        }),
-        mkdirSync: jest.fn(),
-        writeFileSync: jest.fn(),
+        })
     }
 });
 
@@ -34,30 +54,76 @@ describe('UpdateEndpoint', () => {
         updateEndpoint = new UpdateEndpoint();
     });
 
-    it('can recieve an update', () => {
-        (fs as any).shouldFail(false);
-        const payload = "{}";
+    afterEach(() => {
+        (fs as any).restore();
+    })
 
-        updateEndpoint.put(payload, mockClient, mockServer)
+    describe('put', () => {
+        it('can update the file', () => {
+            const payload = "{}";
+    
+            updateEndpoint.put(payload, mockClient, mockServer)
+    
+            expect(fs.existsSync).toHaveBeenCalledWith(DATA_DIR_PATH);            
+            expect(fs.writeFileSync).toHaveBeenCalledWith(DATA_FILE_PATH, payload);
+            expect(mockClient.onSuccess).toHaveBeenCalledWith(payload);
+            expect(mockServer.io.emit).toHaveBeenCalledWith('update', payload);
+        });
+    
+        it('will make the directory if it doesnt exist', () => {
+            (fs as any).fileExists(false);
+    
+            updateEndpoint.put(null, mockClient, mockServer)
+    
+            expect(fs.mkdirSync).toHaveBeenCalledWith(DATA_DIR_PATH);
+        });
+    
+        it('will emit error to socket on failure', (done) => {
+            (fs as any).throwsError();
+            try {
+                updateEndpoint.put("{}", mockClient, mockServer)
+    
+                expect(fs.existsSync).toHaveBeenCalledWith(DATA_DIR_PATH);
+                expect(mockClient.onError).toHaveBeenCalledWith(expect.any(String));
+                expect(console.log).toHaveBeenCalled();
+                done();
+            } catch (e) {
+                done.fail(e)
+            }
+        });
+    })
 
-        expect(fs.existsSync).toHaveBeenCalledWith(DATA_DIR_PATH);            
-        expect(fs.mkdirSync).toHaveBeenCalledWith(DATA_DIR_PATH);
-        expect(fs.writeFileSync).toHaveBeenCalledWith(DATA_FILE_PATH, payload);
-        expect(mockClient.onSuccess).toHaveBeenCalledWith(payload);
-        expect(mockServer.io.emit).toHaveBeenCalledWith('update', payload);
-    });
+    
 
-    it('will emit error to socket on update failure', (done) => {
-        (fs as any).shouldFail();
-        try {
-            updateEndpoint.put("{}", mockClient, mockServer)
+    describe('get', () => {
+        it('can return the file contents', () => {
+            const expectedPayload = JSON.stringify({ test: 1 })
+            updateEndpoint.get("", mockClient, null);
+            expect(mockClient.onSuccess).toHaveBeenCalledWith(expectedPayload);
+        })
 
-            expect(fs.existsSync).toHaveBeenCalledWith(DATA_DIR_PATH);
+        it('can return a value to user from key', () => {
+            const payload = { key: 'test' }
+            updateEndpoint.get(JSON.stringify(payload), mockClient, null);
+            expect(mockClient.onSuccess).toHaveBeenCalledWith("1");
+        })
+
+        it('can report on error for invalid key', () => {
+            const payload = { key: 'invalid' }
+            updateEndpoint.get(JSON.stringify(payload), mockClient, null);
             expect(mockClient.onError).toHaveBeenCalledWith(expect.any(String));
-            expect(console.log).toHaveBeenCalled();
-            done();
-        } catch (e) {
-            done.fail(e)
-        }
-    });
+        })
+
+        it('can report on error for invalid file', () => {
+            (fs as any).fileIsValid(false);
+            updateEndpoint.get(null, mockClient, null);
+            expect(mockClient.onError).toHaveBeenCalledWith(expect.any(String));
+        })
+        
+        it('can report on error for non existing file', () => {
+            (fs as any).fileExists(false);
+            updateEndpoint.get(null, mockClient, null);
+            expect(mockClient.onError).toHaveBeenCalledWith(expect.any(String));
+        })
+    })
 })
